b04cd3f09c024231c9301737b3b978cd0a4535f1
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-04-03 19:00:28 -0400

    ogrStream, leaning on duplex-child-process for a nice api

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
 var wrap = require('../lib/wrapCSV.js');
+var ogrStream= require('../lib/ogrStream.js');
 
 wrap('test/data/t.csv','LAT','LONG','NAD83')
+  .pipe(ogrStream())
   .pipe(process.stdout);
