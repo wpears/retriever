5a299b6e19e0cb29be9e2fcafb30214d0d8c44bb
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2015-04-03 17:51:01 -0400

    outline

---
+++ retriever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var program = require('commander');
+
+//read data list
+//  this includes operations that need to be performed
+//request data by line
+//transform data as needed
+//save to S3 bucket
